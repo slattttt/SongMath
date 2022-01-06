f31c618e9076a40af08ace363c4e5b4ec673836d
slattttt <89854587+slattttt@users.noreply.github.com>
2022-01-06 09:31:11 -0500

    Initial commit

---
+++ src/main/java/SongMath.java
@@ -0,0 +1,10 @@
+//remember imports
+
+public class SongMath {
+    public static void main(String[] args) {
+
+        //remember FileReader, BufferedReader for reading
+        //FileWriter, BufferedWriter for writing
+
+    }//end of main
+}//end of class
